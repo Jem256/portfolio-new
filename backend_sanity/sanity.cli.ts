4c12cb3fdba70889bdfb4be2ce7ec5ef1acbb761
Jemimah Nagasha <jemynagasha@gmail.com>
2023-07-24 19:57:10 +0300

    backend and frontend setup

---
+++ backend_sanity/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'qaz75joi',
+    dataset: 'production'
+  }
+})
---
+++ backend_sanity/schemas/index.ts
@@ -0,0 +1,13 @@
+// import schema types 
+import works from './works'
+import brands from './brands'
+import abouts from './abouts'
+import experiences from './experiences'
+import skills from './skills'
+import workExperience from './workExperience'
+import contact from './contact'
+import testimonials from "./testimonials"
+
+export const schemaTypes = [
+	works, testimonials, brands, abouts, skills, workExperience, experiences, contact
+]
